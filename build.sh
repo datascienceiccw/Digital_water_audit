454ff38851a6f5dcc26686b527f4cbe8c89d72ce
Viraj Sharma <viraj19r@gmail.com>
2024-03-20 12:39:04 +0530

    update deployment settings

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 # Build the project
 echo "Building the project"
-python3.11 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Migrations
 echo "Making Migrations"
-python3.11 manage.py makemigrations --noinput
-python3.11 manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 echo "Collecting Static Files"
-python3.11 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
