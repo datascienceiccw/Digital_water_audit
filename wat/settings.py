ab3daa054df80db7f07e3ff81d724475d102b888
Viraj Sharma <viraj19r@gmail.com>
2024-03-20 15:16:52 +0530

    setup railway config

--- wat/settings.py
+++ wat/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static' 
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'frontend/build/static/css'), os.path.join(BASE_DIR, 'frontend/build/static/js') ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
